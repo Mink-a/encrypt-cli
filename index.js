70572feb835a2332fe56d6f9f4b05ab4edce8a8c
mkk <minkhant.kyaw@kbzbank.com>
2024-07-10 13:28:44 +0630

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import { program } from "commander";
+import Aes from "aes-everywhere";
+const { encrypt, decrypt } = Aes;
+
+program
+  .version("1.0.0")
+  .description("Aes-Everywhere CLI")
+  .option("-k, --key <string>", "Encryption key")
+  .option("-t, --text <string>", "Text to encrypt or decrypt")
+  .option("-a, --action <string>", "Action to perform (encrypt or decrypt)")
+  .action((options) => {
+    if (!options.enKey) {
+      console.log("Encryption key is required");
+      process.exit(1);
+    }
+    if (!options.action) {
+      console.log("Action is required");
+      process.exit(1);
+    }
+    if (!options.text) {
+      console.log("Text is required");
+      process.exit(1);
+    }
+    if (options.action !== "encrypt" && options.action !== "decrypt") {
+      console.log("Action must be either encrypt or decrypt");
+      process.exit(1);
+    }
+    if (options.action === "encrypt") {
+      const res = encrypt(options.text, options.enKey);
+      console.log(res);
+    } else {
+      const res = decrypt(options.text, options.enKey);
+      console.log(res);
+    }
+  });
+
+program.parse(process.argv);
